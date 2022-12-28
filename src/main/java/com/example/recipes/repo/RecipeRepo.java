446e33ce8651770eeff7979aa284ef2085acb176
olamide <myyde8170@gmail.com>
2022-12-28 06:11:30 +0100

    Repository for recipes

--- src/main/java/com/example/recipes/repo/RecipeRepo.java
+++ src/main/java/com/example/recipes/repo/RecipeRepo.java
@@ -2,6 +2,8 @@ package com.example.recipes.repo;
 
 import com.example.recipes.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeRepo extends JpaRepository<Recipe, Long> {
 }
