40bea4d50943f434af6417c6775b6f96e5a9d91d
olamide <myyde8170@gmail.com>
2022-12-29 16:29:42 +0100

    Made Recipe an entity and added ElementCollection to array of ingredients and directions

--- src/main/java/com/example/recipes/model/Recipe.java
+++ src/main/java/com/example/recipes/model/Recipe.java
@@ -1,17 +1,25 @@
 package com.example.recipes.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private String description;
+
+    @ElementCollection
     private List<String> ingredients;
+
+    @ElementCollection
     private List<String> directions;
 }
