38c77cec5e3ef3ef33cf01f127483e04b97440cc
olamide <myyde8170@gmail.com>
2022-12-29 16:31:46 +0100

    added methods to addRecipe and getRecipeById

--- src/main/java/com/example/recipes/service/RecipeService.java
+++ src/main/java/com/example/recipes/service/RecipeService.java
@@ -1,19 +1,33 @@
 package com.example.recipes.service;
 
 import com.example.recipes.model.Recipe;
-import lombok.AllArgsConstructor;
+import com.example.recipes.repo.RecipeRepo;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collections;
+import java.util.Map;
 
 @Service
 public class RecipeService {
-    private Recipe recipe;
+    private RecipeRepo recipeRepo;
 
-    public Recipe getRecipe() {
-        return recipe;
+    public RecipeService(RecipeRepo recipeRepo) {
+        this.recipeRepo = recipeRepo;
     }
 
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
+    public Recipe getRecipeById(Long id) {
+        return recipeRepo.findById(id).orElseThrow(() -> new
+                ResponseStatusException(HttpStatus.NOT_FOUND));
     }
+
+    public Map addRecipe(Recipe recipe) {
+        recipeRepo.save(recipe);
+
+         // Return the id of the new recipe in the response
+        return Collections.singletonMap("id", recipe.getId());
+    }
+
 
 }
