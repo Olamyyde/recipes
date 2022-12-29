f9414cc02a0d9b30e9a12ca3a7abc1b36ca2b68d
olamide <myyde8170@gmail.com>
2022-12-29 18:26:17 +0100

    implementation for deleting recipes

--- src/main/java/com/example/recipes/service/RecipeService.java
+++ src/main/java/com/example/recipes/service/RecipeService.java
@@ -3,11 +3,13 @@ package com.example.recipes.service;
 import com.example.recipes.model.Recipe;
 import com.example.recipes.repo.RecipeRepo;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class RecipeService {
@@ -29,5 +31,14 @@ public class RecipeService {
         return Collections.singletonMap("id", recipe.getId());
     }
 
+    public ResponseEntity deleteRecipe(Long id){
+        Optional<Recipe> optionalRecipe = recipeRepo.findById(id);
+        Recipe recipe = optionalRecipe.get();
+        if (recipe == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        recipeRepo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
