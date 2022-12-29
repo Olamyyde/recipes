8ad362b30b873066a1fec8d7a825fec3870a0b92
olamide <myyde8170@gmail.com>
2022-12-29 16:33:27 +0100

    added endpoints to add a new Recipe and get a Recipe By Id

--- src/main/java/com/example/recipes/controller/RecipeController.java
+++ src/main/java/com/example/recipes/controller/RecipeController.java
@@ -2,23 +2,39 @@ package com.example.recipes.controller;
 
 import com.example.recipes.model.Recipe;
 import com.example.recipes.service.RecipeService;
-import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/recipe")
-@AllArgsConstructor
 public class RecipeController {
     private final RecipeService recipeService;
 
-    @PostMapping
-    public void addRecipe(@RequestBody Recipe recipe){
-        recipeService.setRecipe(recipe);
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
-    @GetMapping
-    public Recipe getRecipe(){
-        return recipeService.getRecipe();
+    @PostMapping("/new")
+    public ResponseEntity<Map<String, Long>> addRecipe(@RequestBody Recipe recipe){
+        // Add the recipe to the database
+//        Recipe newRecipe = recipeService.addRecipe(recipe);
+//
+        Map<String, Long> response = recipeService.addRecipe(recipe);
+//        response.put("id", newRecipe.getId());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Recipe> getRecipe(@PathVariable Long id){
+        Recipe recipeById = recipeService.getRecipeById(id);
+//        if (recipeById == null)
+//            return new ResponseEntity<>(recipeById, HttpStatus.NOT_FOUND);
+//        else
+            return new ResponseEntity<>(recipeById, HttpStatus.OK);
     }
 
 
