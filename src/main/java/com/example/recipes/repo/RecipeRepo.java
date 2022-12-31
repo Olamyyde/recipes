bd4093e2835e9db73489cfc03473da35ec792e11
olamide <myyde8170@gmail.com>
2022-12-31 10:24:17 +0100

    added custom jpa methods

--- src/main/java/com/example/recipes/repo/RecipeRepo.java
+++ src/main/java/com/example/recipes/repo/RecipeRepo.java
@@ -4,6 +4,13 @@ import com.example.recipes.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeRepo extends JpaRepository<Recipe, Long> {
+    List<Recipe> findByCategoryIgnoreCaseOrderByDateDesc(String category);
+    List<Recipe> findByNameIgnoreCaseOrderByDateDesc(String name);
+
+    List<Recipe> findAllByOrderByDateDesc();
+
 }
